2ec0873cb404772af8b3e0efe51d525f0a10bef1
joao-batista <joao.stinfo@gmail.com>
2019-04-15 18:03:42 -0300

    services e reporitorios

---
+++ src/main/java/com/spring/pontointeligente/api/entities/Lancamento.java
@@ -0,0 +1,135 @@
+package com.spring.pontointeligente.api.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import com.spring.pontointeligente.api.enums.TipoEnum;
+
+@Entity
+@Table(name = "lancamento")
+public class Lancamento implements Serializable {
+
+	private static final long serialVersionUID = 6524560251526772839L;
+
+	private Long id;
+	private Date data;
+	private String descricao;
+	private String localizacao;
+	private Date dataCriacao;
+	private Date dataAtualizacao;
+	private TipoEnum tipo;
+	private Funcionario funcionario;
+
+	public Lancamento() {
+	}
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	public Long getId() {
+		return this.id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "data", nullable = false)
+	public Date getData() {
+		return this.data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+
+	@Column(name = "descricao", nullable = true)
+	public String getDescricao() {
+		return this.descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	@Column(name = "localizacao", nullable = true)
+	public String getLocalizacao() {
+		return this.localizacao;
+	}
+
+	public void setLocalizacao(String localizacao) {
+		this.localizacao = localizacao;
+	}
+
+	@Column(name = "data_criacao", nullable = false)
+	public Date getDataCriacao() {
+		return this.dataCriacao;
+	}
+
+	public void setDataCriacao(Date dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
+
+	@Column(name = "data_atualizacao", nullable = false)
+	public Date getDataAtualizacao() {
+		return this.dataAtualizacao;
+	}
+
+	public void setDataAtualizacao(Date dataAtualizacao) {
+		this.dataAtualizacao = dataAtualizacao;
+	}
+
+	@Enumerated(EnumType.STRING)
+	@Column(name = "tipo", nullable = false)
+	public TipoEnum getTipo() {
+		return this.tipo;
+	}
+
+	public void setTipo(TipoEnum tipo) {
+		this.tipo = tipo;
+	}
+
+	@ManyToOne(fetch = FetchType.EAGER)
+	public Funcionario getFuncionario() {
+		return this.funcionario;
+	}
+
+	public void setFuncionario(Funcionario funcionario) {
+		this.funcionario = funcionario;
+	}
+
+	@PreUpdate
+	public void preUpdate() {
+		this.dataAtualizacao = new Date();
+	}
+
+	@PrePersist
+	public void prePersist() {
+		final Date atual = new Date();
+		this.dataCriacao = atual;
+		this.dataAtualizacao = atual;
+	}
+
+	@Override
+	public String toString() {
+		return "Lancamento [id=" + this.id + ", data=" + this.data + ", descricao=" + this.descricao + ", localizacao=" + this.localizacao
+				+ ", dataCriacao=" + this.dataCriacao + ", dataAtualizacao=" + this.dataAtualizacao + ", tipo=" + this.tipo
+				+ ", funcionario=" + this.funcionario + "]";
+	}
+
+}
